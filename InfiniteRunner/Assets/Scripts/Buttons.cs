468fe8c92f76a8d27385e9d1a2f0746fd6949a4b
Conner Anderson <21.conner.anderson@ksd.org>
2020-02-11 09:28:34 -0800

    Cannon features

---
+++ InfiniteRunner/Assets/Scripts/Buttons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Buttons : MonoBehaviour
+{
+    public Button fireButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StopButton()
+    {
+        Cannon.rotating = false;
+        Cannon.power = true;
+        Destroy(this.gameObject);
+        fireButton.GetComponent<Image>().enabled = true;
+    }
+    public void PowerButton()
+    {
+        Cannon.power = false;
+        Destroy(this.gameObject);
+    }
+}
---
+++ InfiniteRunner/Assets/Scripts/Cannon.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cannon : MonoBehaviour
+{
+    static public bool rotating = true;
+    static public bool power = false;
+    bool onUp = true;
+    bool powerUp = true;
+    int i = 0;
+    public int speed = 0;
+    public Vector2 velocity;
+    public Slider slider;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotating)
+        {
+            if (onUp)
+            {
+                transform.Rotate(0, 0, 1);
+                i++;
+                if (i > 55)
+                {
+                    onUp = false;
+                    i = 0;
+                }
+            }
+            else
+            {
+                transform.Rotate(0, 0, -1);
+                i++;
+                if (i > 55)
+                {
+                    onUp = true;
+                    i = 0;
+                }
+            }
+        }
+        if (power)
+        {
+            if (powerUp)
+            {
+                slider.value += Time.deltaTime;
+                if (slider.value == 1)
+                {
+                    powerUp = false;
+                }
+            }
+            else
+            {
+                slider.value -= Time.deltaTime;
+                if (slider.value == 0)
+                {
+                    powerUp = true;
+                }
+            }
+        }
+        if (rotating == false && power == false)
+        {
+            player.transform.rotation = transform.rotation;
+            player.GetComponent<SpriteRenderer>().enabled = true;
+            player.GetComponent<Rigidbody2D>().gravityScale = 50;
+            player.GetComponent<Rigidbody2D>().velocity = player.transform.right * speed * slider.value;
+        }
+        player.GetComponent<Rigidbody2D>().velocity = player.GetComponent<Rigidbody2D>().velocity * .1f;
+        velocity = player.GetComponent<Rigidbody2D>().velocity;
+    }
+}
