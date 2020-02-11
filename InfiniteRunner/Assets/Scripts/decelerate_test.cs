caa4d7a76da9e256d8f93881fd2395c39c174627
Jayden Connerly <18.jayden.connerly@ksd.org>
2020-02-11 09:51:52 -0800

    arc testing

---
+++ InfiniteRunner/Assets/Scripts/decelerate_test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class decelerate_test : MonoBehaviour
+{
+    public float power;
+    public float decel_factor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 launch = new Vector2(1, 1);
+        launch.Normalize();
+        GetComponent<Rigidbody2D>().velocity = launch * power;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity * (1.0f / decel_factor);
+    }
+}
