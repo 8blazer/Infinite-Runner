ecb29357f0ec1d35f8fd4afc63cc1b53b429ade1
Conner Anderson <21.conner.anderson@ksd.org>
2020-02-12 08:51:12 -0800

    Improved fish

---
+++ InfiniteRunner/Assets/Scripts/Running.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Running : MonoBehaviour
+{
+    float accTimer = 0;
+    float oldX = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > -4.5f)
+        {
+            accTimer += Time.deltaTime;
+            if (accTimer > 1)
+            {
+                if (oldX + .1f > transform.position.x)
+                {
+                    transform.Rotate(0, 0, transform.rotation.z * -1);
+                }
+                accTimer = 0;
+                oldX = transform.position.x;
+            }
+        }
+    }
+}
