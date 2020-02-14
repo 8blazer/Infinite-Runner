65512417a48ce667918d457389e0fd26952fb7fa
Jayden Connerly <18.jayden.connerly@ksd.org>
2020-02-14 08:52:58 -0800

    distance counter

---
+++ InfiniteRunner/Assets/Scripts/DistanceCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceCounter : MonoBehaviour
+{
+    public Transform player;
+    public Text distanceText;
+    public int distanceRaw;
+    public int distanceMeter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceRaw = Mathf.RoundToInt(player.position.x);
+        distanceMeter = Mathf.RoundToInt(distanceRaw / 5.0f);
+        distanceText.text = distanceMeter + "m";
+    }
+}
