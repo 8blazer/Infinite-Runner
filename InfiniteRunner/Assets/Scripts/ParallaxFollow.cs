fd710a9f633fd83579057c595278bc0e50741927
Jayden Connerly <18.jayden.connerly@ksd.org>
2020-02-26 09:17:52 -0800

    PARALLAXgit add -Agit add -Agit add -A1

---
+++ InfiniteRunner/Assets/Scripts/ParallaxFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxFollow : MonoBehaviour
+{
+    public Transform cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3(cam.position.x, cam.position.y, transform.position.z);
+        transform.position = pos;
+        
+    }
+}
---
+++ InfiniteRunner/Assets/Scripts/ParallaxLooper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxLooper : MonoBehaviour
+{
+    public Transform source;
+    Vector3 previousSourcePos;
+    public float parallaxScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousSourcePos = source.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float parallax = (previousSourcePos.x - source.position.x)
+            * parallaxScale;
+        Vector3 pos = transform.position;
+        pos.x += parallax;
+        transform.position = pos;
+        if (transform.position.x <= source.position.x -25.425f)
+        {
+            Vector3 pos2 = new Vector3(transform.position.x + 50.85f, transform.position.y, transform.position.z);
+            transform.position = pos2;
+            pos = pos2;
+        }
+        previousSourcePos = source.position;
+    }
+}
